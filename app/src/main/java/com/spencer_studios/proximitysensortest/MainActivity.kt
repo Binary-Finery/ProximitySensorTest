86c6fca091e20d972d904fd6b8b457d32f510acb
Dean Spencer <deanspencer007@gmail.com>
2021-09-20 20:08:01 +0100

    mods and stuff

--- app/src/main/java/com/spencer_studios/proximitysensortest/MainActivity.kt
+++ app/src/main/java/com/spencer_studios/proximitysensortest/MainActivity.kt
@@ -2,17 +2,16 @@ package com.spencer_studios.proximitysensortest
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.media.MediaPlayer
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.airbnb.lottie.LottieAnimationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -20,8 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var sensorManager: SensorManager
     private lateinit var proximitySensor: Sensor
-    private lateinit var lottieView: LottieAnimationView
-    private lateinit var mediaPlayer: MediaPlayer
+    private var mediaPlayer: MediaPlayer? = null
 
     private var color: Int = 0
     private var count: Int = 0
@@ -33,42 +31,31 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         toolbar.visibility = View.GONE
-        lottieView = findViewById(R.id.lottie_not_found)
-
-        playAnimation(false)
-        setEventCount()
-
+        ic_info.setOnClickListener { startActivity(Intent(it.context, InfoActivity::class.java)) }
         ivReset.setOnClickListener {
             count = 0
             setEventCount()
         }
+        playAnimation(false)
+        setEventCount()
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
-        sensorManager.registerListener(
-            proximitySensorEventListener,
-            proximitySensor,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
-
-        setSensorInfo()
+        sensorManager.registerListener(proximitySensorEventListener, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     private var proximitySensorEventListener: SensorEventListener =
         object : SensorEventListener {
             override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
-                //unused method
             }
-
             override fun onSensorChanged(event: SensorEvent) {
-                if (event.sensor.type == Sensor.TYPE_PROXIMITY)
-                    playAnimation(event.values[0] < event.sensor.maximumRange)
+                if (event.sensor.type == Sensor.TYPE_PROXIMITY) playAnimation(event.values[0] < event.sensor.maximumRange)
             }
         }
 
     private fun playAnimation(objectDetected: Boolean) {
         if (objectDetected) {
-            mediaPlayer.start()
+            if (mediaPlayer != null) mediaPlayer?.start()
             color = Color.parseColor("#4CAF50")
             tv.text = getString(R.string.object_detected)
             lottieView.setAnimation("thumbs_up.json")
@@ -88,41 +75,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun setSensorInfo() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            try {
-                tvInfo.text =
-                            "name       : ${proximitySensor.name}\n" +
-                            "vendor     : ${proximitySensor.vendor}\n" +
-                            "power      : ${proximitySensor.power} mA\n" +
-                            "resolution : ${proximitySensor.resolution}"
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun setEventCount() {
-        tvEvents.text = "EVENTS $count"
-    }
-
+    private fun setEventCount() { tvEvents.text = "events $count" }
 
     override fun onStart() {
         super.onStart()
         mediaPlayer = MediaPlayer.create(this, R.raw.chime_1)
-        sensorManager.registerListener(
-            proximitySensorEventListener,
-            proximitySensor,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
+        sensorManager.registerListener(proximitySensorEventListener, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
-    override fun onPause() {
-        super.onPause()
-        mediaPlayer.stop()
-        mediaPlayer.release()
+    override fun onStop() {
+        if (mediaPlayer != null) {
+            mediaPlayer?.stop()
+            mediaPlayer?.release()
+            mediaPlayer = null
+        }
         sensorManager.unregisterListener(proximitySensorEventListener)
+        super.onStop()
     }
 }
